abfabbcba83edcf034d56799f2a59e70d9e2d454
Joao-Baby <jvs17@aluno.ifnmg.edu.br>
2026-04-27 20:20:10 +0000

    chore: resolução dos exercícios da aula 07

--- exercicios/aula07/index.js
+++ exercicios/aula07/index.js
@@ -8,16 +8,22 @@
 // Exercício 1 — objetoParaJSON
 export function objetoParaJSON(objeto) {
   // escreva seu código aqui
+  const textoJSON = JSON.stringify(objeto);
+  return textoJSON;
 }
 
 // Exercício 2 — jsonParaObjeto
 export function jsonParaObjeto(texto) {
   // escreva seu código aqui
+  const objeto = JSON.parse(texto);
+  return objeto;
 }
 
 // Exercício 3 — clonarObjeto
 export function clonarObjeto(objeto) {
   // escreva seu código aqui
+  const objetoClone = structuredClone(objeto);
+  return objetoClone;
 }
 
 // ──────────────────────────────────────────────────
@@ -35,9 +41,21 @@ function simularConsulta(id) {
 // Exercício 4 — buscarDados
 export async function buscarDados(id) {
   // escreva seu código aqui
+  const resultado = await simularConsulta(id);
+  return resultado;
 }
 
 // Exercício 5 — dividirSeguro
 export async function dividirSeguro(a, b) {
   // escreva seu código aqui
+  try { 
+    const alunoA = await buscarDados(a);
+    const alunoB = await buscarDados(b);
+    if (alunoB.id === 0) {
+      throw new Error("Divisão por zero"); 
+    }
+    return alunoA.id / alunoB.id;
+  } catch (erro) {
+    throw new Error("Divisão por zero");
+  }
 }
